3e4ad351d2a25df2d8643a8c417442c535ac3337
wusimin7 <wusimin7@jd.com>
2021-01-31 19:11:15 +0800

    chore: 项目初始化

---
+++ JS/prototype.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-31 19:10:24
+ * @LastEditTime: 2021-01-31 19:10:24
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vcreact/Users/wusimin7/Desktop/qihang__crous/JS/prototype.js
+ */
